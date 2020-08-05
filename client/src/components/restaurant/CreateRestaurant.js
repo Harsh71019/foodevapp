22cf3e401b20cea9d0e860d0f4adb0b3a7b7b9f6
harsh <harsh710pat@gmail.com>
2020-08-06 00:20:45 +0530

    Hey Internshala Code Completed

--- client/src/components/restaurant/CreateRestaurant.js
+++ client/src/components/restaurant/CreateRestaurant.js
@@ -112,7 +112,7 @@ class CreateRestaurant extends Component {
                   marginTop: "1rem"
                 }}
                 type="submit"
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                className="btn btn-large waves-effect waves-light hoverable black accent-3"
               >
                 Create Restaurant
               </button>
